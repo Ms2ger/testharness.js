38ba3b87bfb2fec61de608247bfd73a5e2a05262
Ms2ger <ms2ger@gmail.com>
2013-10-18 20:58:12 +0200

    Comment out a querySelectorAll call.

--- testharness.js
+++ testharness.js
@@ -1787,6 +1787,7 @@ policies and contribution forms [3].
 
         log.appendChild(render(summary_template, {num_tests:tests.length}, output_document));
 
+        /* XXX #851
         forEach(output_document.querySelectorAll("section#summary label"),
                 function(element)
                 {
@@ -1811,7 +1812,7 @@ policies and contribution forms [3].
                                      style_element.parentNode.removeChild(style_element);
                                  }
                              });
-                });
+                });*/
 
         // This use of innerHTML plus manual escaping is not recommended in
         // general, but is necessary here for performance.  Using textContent
